a1064cd25f4d0ffd4a98eafcc2c52c672a607259
slembcke <slembcke@gmail.com>
2012-12-17 15:22:17 -0600

    Setting up project

--- Gemeralds/AppDelegate.m
+++ Gemeralds/AppDelegate.m
@@ -171,11 +171,4 @@
 	[[CCDirector sharedDirector] setNextDeltaTimeZero:YES];
 }
 
-- (void) dealloc
-{
-	[window_ release];
-	[navController_ release];
-	
-	[super dealloc];
-}
 @end
--- Gemeralds/HelloWorldLayer.m
+++ Gemeralds/HelloWorldLayer.m
@@ -72,8 +72,6 @@
 			AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
 			
 			[[app navController] presentModalViewController:achievementViewController animated:YES];
-			
-			[achievementViewController release];
 		}
 									   ];
 
@@ -87,8 +85,6 @@
 			AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
 			
 			[[app navController] presentModalViewController:leaderboardViewController animated:YES];
-			
-			[leaderboardViewController release];
 		}
 									   ];
 		
@@ -102,17 +98,6 @@
 
 	}
 	return self;
-}
-
-// on "dealloc" you need to release all your retained objects
-- (void) dealloc
-{
-	// in case you have something to dealloc, do it in this method
-	// in this particular example nothing needs to be released.
-	// cocos2d will automatically release all the children (Label)
-	
-	// don't forget to call "super dealloc"
-	[super dealloc];
 }
 
 #pragma mark GameKit delegate
